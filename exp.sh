42fdb76416177642e76403d661d81078bd7b1029
dgymjol <dgymjol@yonsei.ac.kr>
2023-09-01 18:38:18 +0900

    just test

--- exp.sh
+++ exp.sh
@@ -1,6 +1,12 @@
 # for debugging
-CUDA_VISIBLE_DEVICES=1 python train.py
+# CUDA_VISIBLE_DEVICES=1 python train.py
 
 
 # for ddp training
-python3 -m torch.distributed.launch --nproc_per_node=2 train.py
+# python3 -m torch.distributed.launch --nproc_per_node=2 train.py
+
+
+
+# cd experiment/deeplabv3_ddp_irn_bdcl_test
+# python3 -m torch.distributed.launch --nproc_per_node=2 train.py
+# python test.py
