4ec76fa03df6a41c19bc642e2cc0670131d65395
dgymjol <dgymjol@yonsei.ac.kr>
2023-09-01 18:38:52 +0900

    just test2

--- exp.sh
+++ exp.sh
@@ -7,6 +7,13 @@
 
 
 
-# cd experiment/deeplabv3_ddp_irn_bdcl_test
+cd experiment/deeplabv3_ddp_irn_bdcl_test
 # python3 -m torch.distributed.launch --nproc_per_node=2 train.py
 # python test.py
+
+cd ../../
+# mv log/logfile.txt log/deeplabv3_ddp_irn_bdcl_test/logfile.txt
+
+git add .
+git commit -m "just test2"
+git push
